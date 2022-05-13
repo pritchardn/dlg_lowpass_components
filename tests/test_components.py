ad300d0a3f99f1ea1de0fa389505c36843e14352
pritchardn <21726929@student.uwa.edu.au>
2022-05-13 12:34:19 +0800

    Cleans up test implementation

--- tests/test_components.py
+++ tests/test_components.py
@@ -4,27 +4,26 @@ from dlg_lowpass_components import LPSignalGenerator, LPWindowGenerator, LPAddNo
 from dlg.drop import InMemoryDROP
 from dlg.droputils import allDropContents, DROPWaiterCtx
 
+def _run_component(component):
+    memory = InMemoryDROP("b", "b")
+    component.addOutput(memory)
+    component.run()
+    memory.setCompleted()
+    return allDropContents(memory)
+
 
 class TestLPSignalGenerator(unittest.TestCase):
 
     def test_default(self):
         generator = LPSignalGenerator("a", "a")
-        memory = InMemoryDROP("b", "b")
-        generator.addOutput(memory)
-        generator.run()
-        memory.setCompleted()
-        signal = allDropContents(memory)
+        signal = _run_component(generator)
         self.assertIsNotNone(signal)
         self.assertEqual(generator.length * 8, len(signal))
 
     def test_changed_length(self):
         new_length = 1024
         generator = LPSignalGenerator("a", "a", length=new_length)
-        memory = InMemoryDROP("b", "b")
-        generator.addOutput(memory)
-        generator.run()
-        memory.setCompleted()
-        signal = allDropContents(memory)
+        signal = _run_component(generator)
         self.assertIsNotNone(signal)
         self.assertEqual(new_length, generator.length)
         self.assertEqual(new_length * 8, len(signal))
@@ -33,16 +32,8 @@ class TestLPSignalGenerator(unittest.TestCase):
         noise_params = {'mean': 0.0, 'std': 1.0, 'freq': 666, 'seed': 42}
         noisy_generator = LPSignalGenerator("a", "a", noise=noise_params)
         vanilla_generator = LPSignalGenerator("A", "A")
-        noisy_memory = InMemoryDROP("b", "b")
-        vanilla_memory = InMemoryDROP("B", "B")
-        noisy_generator.addOutput(noisy_memory)
-        vanilla_generator.addOutput(vanilla_memory)
-        noisy_generator.run()
-        vanilla_generator.run()
-        noisy_memory.setCompleted()
-        vanilla_memory.setCompleted()
-        noisy_signal = allDropContents(noisy_memory)
-        vanilla_signal = allDropContents(vanilla_memory)
+        noisy_signal = _run_component(noisy_generator)
+        vanilla_signal = _run_component(vanilla_generator)
         self.assertIsNotNone(noisy_signal)
         self.assertIsNotNone(vanilla_signal)
         self.assertNotEqual(noisy_signal, vanilla_signal)
@@ -50,17 +41,9 @@ class TestLPSignalGenerator(unittest.TestCase):
     def test_changed_frequency(self):
         new_frequency = {'values': [440, 900, 1100, 1900, 2400]}
         generator = LPSignalGenerator("a", "a", freqs=new_frequency)
-        memory = InMemoryDROP("b", "b")
         vanilla_generator = LPSignalGenerator("A", "A")
-        vanilla_memory = InMemoryDROP("B", "B")
-        generator.addOutput(memory)
-        vanilla_generator.addOutput(vanilla_memory)
-        generator.run()
-        vanilla_generator.run()
-        memory.setCompleted()
-        vanilla_memory.setCompleted()
-        signal = allDropContents(memory)
-        vanilla_signal = allDropContents(vanilla_memory)
+        signal = _run_component(generator)
+        vanilla_signal = _run_component(vanilla_generator)
 
         self.assertIsNotNone(signal)
         self.assertIsNotNone(vanilla_signal)
@@ -70,55 +53,37 @@ class TestLPWindowGenerator(unittest.TestCase):
 
     def test_default(self):
         generator = LPWindowGenerator("a", "a")
-        memory = InMemoryDROP("b", "b")
-        generator.addOutput(memory)
-        generator.run()
-        memory.setCompleted()
-        signal = allDropContents(memory)
+        signal = _run_component(generator)
         self.assertIsNotNone(signal)
         self.assertEqual(generator.length * 8, len(signal))
 
     def test_change_length(self):
         new_length = 512
         generator = LPWindowGenerator("a", "a", length=new_length)
-        memory = InMemoryDROP("b", "b")
-        generator.addOutput(memory)
-        generator.run()
-        memory.setCompleted()
-        signal = allDropContents(memory)
+        signal = _run_component(generator)
         self.assertIsNotNone(signal)
         self.assertEqual(new_length, generator.length)
         self.assertEqual(new_length * 8, len(signal))
 
     def test_change_cutoff(self):
         new_cutoff = 660
-        generator = LPWindowGenerator("a", "a", freqs=new_cutoff)
-        memory = InMemoryDROP("b", "b")
+        generator = LPWindowGenerator("a", "a", cutoff=new_cutoff)
         vanilla_generator = LPWindowGenerator("A", "A")
-        vanilla_memory = InMemoryDROP("B", "B")
-        generator.addOutput(memory)
-        vanilla_generator.addOutput(vanilla_memory)
-        generator.run()
-        vanilla_generator.run()
-        memory.setCompleted()
-        vanilla_memory.setCompleted()
-        signal = allDropContents(memory)
-        vanilla_signal = allDropContents(vanilla_memory)
+        signal = _run_component(generator)
+        vanilla_signal = _run_component(vanilla_generator)
+        self.assertIsNotNone(signal)
+        self.assertIsNotNone(vanilla_signal)
+        self.assertNotEqual(signal, vanilla_signal)
 
     def test_change_srate(self):
         new_srate = 4500
         generator = LPWindowGenerator("a", "a", srate=new_srate)
-        memory = InMemoryDROP("b", "b")
         vanilla_generator = LPWindowGenerator("A", "A")
-        vanilla_memory = InMemoryDROP("B", "B")
-        generator.addOutput(memory)
-        vanilla_generator.addOutput(vanilla_memory)
-        generator.run()
-        vanilla_generator.run()
-        memory.setCompleted()
-        vanilla_memory.setCompleted()
-        signal = allDropContents(memory)
-        vanilla_signal = allDropContents(vanilla_memory)
+        signal = _run_component(generator)
+        vanilla_signal = _run_component(vanilla_generator)
+        self.assertIsNotNone(signal)
+        self.assertIsNotNone(vanilla_signal)
+        self.assertNotEqual(signal, vanilla_signal)
 
 
 class TestLPNoiseGenerator(unittest.TestCase):
@@ -126,11 +91,7 @@ class TestLPNoiseGenerator(unittest.TestCase):
     def test_compare_to_SignalGen(self):
         noise_params = {'mean': 0.0, 'std': 1.0, 'freq': 1200, 'seed': 42}
         noisy_generator = LPSignalGenerator("a", "a", noise=noise_params)
-        noisy_memory = InMemoryDROP("b", "b")
-        noisy_generator.addOutput(noisy_memory)
-        noisy_generator.run()
-        noisy_memory.setCompleted()
-        noisy_signal = allDropContents(noisy_memory)
+        noisy_signal = _run_component(noisy_generator)
 
         vanilla_generator = LPSignalGenerator("A", "A")
         interim_mem = InMemoryDROP("B", "B")
