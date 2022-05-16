47ac59dded1439a5b3496b49d82f7813a0e6a7d8
pritchardn <21726929@student.uwa.edu.au>
2022-05-16 10:45:56 +0800

    Adds author_email to setup.py

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="pritchardn",
+    author_email="nicholas.pritchard@uwa.edu.au",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
